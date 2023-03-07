6f46c5c9c650047ae5c71421358acbf0a67a6057
Kweebac <06.amarshall@gmail.com>
2023-03-07 15:34:39 +0000

    Changed the live and repo look, and added underline on hover

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-const LINK = document.querySelector("a");
-LINK.addEventListener("mouseenter", () => {
-  LINK.style.textDecoration = "underline";
+const LINKS = document.querySelectorAll("a");
+LINKS.forEach((link) => {
+  link.addEventListener("mouseenter", () => {
+    link.style.textDecoration = "underline";
+  });
+  link.addEventListener("mouseleave", () => {
+    link.style.textDecoration = "none";
+  });
 });
-LINK.addEventListener("mouseleave", () => {
-  LINK.style.textDecoration = "none";
-});
-
-// Bideford College — graduated 06/2020 with the following grades:
-// • Combined science: trilogy 5-5
-// • Mathematics 4
-// • Geography 4
-// • English language 3 (spoken language pass)
-// • English literature 3
-// • Business 2
-// • German 2
-// • IT 1
-
-// Petroc (North Devon campus) — graduated 06/2021 with the following grades:
-// • English language 7 (Spoken language pass)
-
-// Army Foundation College — graduated 06/02/2022 with the following grades:
-// • ICT 2
