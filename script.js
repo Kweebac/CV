7af6ce40bf318ebb8a70fdfbc7f6e763a6ff0934
Kweebac <06.amarshall@gmail.com>
2023-03-07 14:38:02 +0000

    Added code for each box, and almost finished the work experience & education sections

---
+++ script.js
@@ -0,0 +1,23 @@
+const LINK = document.querySelector("a");
+LINK.addEventListener("mouseenter", () => {
+  LINK.style.textDecoration = "underline";
+});
+LINK.addEventListener("mouseleave", () => {
+  LINK.style.textDecoration = "none";
+});
+
+// Bideford College — graduated 06/2020 with the following grades:
+// • Combined science: trilogy 5-5
+// • Mathematics 4
+// • Geography 4
+// • English language 3 (spoken language pass)
+// • English literature 3
+// • Business 2
+// • German 2
+// • IT 1
+
+// Petroc (North Devon campus) — graduated 06/2021 with the following grades:
+// • English language 7 (Spoken language pass)
+
+// Army Foundation College — graduated 06/02/2022 with the following grades:
+// • ICT 2
