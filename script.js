38244a3a71b2509ce9b5a74908edfbafb487e257
Kweebac <06.amarshall@gmail.com>
2023-03-08 03:38:19 +0000

    Reworded text

--- script.js
+++ script.js
@@ -16,27 +16,13 @@ function changeCircleColor(num, CIRCLES) {
 
 let circles = [2, 3, 2, 1, 3, 4, 3];
 
-const GIT_CIRCLES = document.querySelectorAll(
-  ".circle-container.git .circlebox .circle"
-);
-const HTML_CIRCLES = document.querySelectorAll(
-  ".circle-container.html .circlebox .circle"
-);
-const JS_CIRCLES = document.querySelectorAll(
-  ".circle-container.js .circlebox .circle"
-);
-const CPP_CIRCLES = document.querySelectorAll(
-  ".circle-container.cpp .circlebox .circle"
-);
-const PROBLEM_CIRCLES = document.querySelectorAll(
-  ".circle-container.problem .circlebox .circle"
-);
-const ATTENTION_CIRCLES = document.querySelectorAll(
-  ".circle-container.attention .circlebox .circle"
-);
-const COMMUNICATION_CIRCLES = document.querySelectorAll(
-  ".circle-container.communication .circlebox .circle"
-);
+const GIT_CIRCLES = document.querySelectorAll(".git .circle");
+const HTML_CIRCLES = document.querySelectorAll(".html .circle");
+const JS_CIRCLES = document.querySelectorAll(".js .circle");
+const CPP_CIRCLES = document.querySelectorAll(".cpp .circle");
+const PROBLEM_CIRCLES = document.querySelectorAll(".problem .circle");
+const ATTENTION_CIRCLES = document.querySelectorAll(".attention .circle");
+const COMMS_CIRCLES = document.querySelectorAll(".comms .circle");
 
 changeCircleColor(0, GIT_CIRCLES);
 changeCircleColor(1, HTML_CIRCLES);
@@ -44,4 +30,4 @@ changeCircleColor(2, JS_CIRCLES);
 changeCircleColor(3, CPP_CIRCLES);
 changeCircleColor(4, PROBLEM_CIRCLES);
 changeCircleColor(5, ATTENTION_CIRCLES);
-changeCircleColor(6, COMMUNICATION_CIRCLES);
+changeCircleColor(6, COMMS_CIRCLES);
